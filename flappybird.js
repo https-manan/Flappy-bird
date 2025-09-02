4d8a0ae9f4bde9465b93585287bf81b5b9622222
Manan <Mananbhardwaj2705@gmail.com>
2025-09-02 11:11:03 +0530

    completed.

--- flappybird.js
+++ flappybird.js
@@ -29,9 +29,12 @@ let pipeY = 0;
 let topPipeImage;
 let bottomPipeImage;
 
-// physics
-let velocityX = -2;//This is for the pipes
-let velocityY = 0 //This is for the bird to go up and down
+
+let velocityX = -2;//This is for the pipes moving left
+let velocityY = 0 //This is for the bird to go up and down (which is gonna be 0 initially)
+let gravity = 0.4;
+let gameOver = false
+
 
 window.onload = () => {
   board = document.getElementById('board');  
@@ -54,28 +57,35 @@ window.onload = () => {
 
   requestAnimationFrame(update);
   setInterval(placePipes, 1500);
-  document.addEventListener('keydown',(e)=>{if(e.code =='space'|| e.code == 'ArrowUp') velocityY = -6})
+  document.addEventListener('keydown',(e)=>{if(e.code =='Space'|| e.code == 'ArrowUp') velocityY = -6})
 };
 
 function update() {
+  if  (gameOver) return;
   requestAnimationFrame(update);
   context.clearRect(0, 0, board.width, board.height);
 
   // bird
-  bird.y += velocityY;8 
+  velocityY+= gravity
+  bird.y = Math.max(bird.y+velocityY,0);//so that the max height the bird can reach is top of frame
   context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);//The crealRect will clear full cacnva so we haev to repaint the bird
+
+  if(bird.y>board.height){
+    gameOver = true;
+  }
 
   // pipes
   for (let i = 0; i < pipeArray.length; i++) {
     let pipe = pipeArray[i];
     pipe.x += velocityX; 
     context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height); 
+    if(detectCollision(bird,pipe)) gameOver = true;
   }
 }
 
 function placePipes() {
   let randomPipeY = pipeY - pipeHeight/5 - Math.random() * (pipeHeight/2);
-  let opeaningSpace = board.height/4;
+  let opeaningSpace = board.height/5;
   let topPipe = {
     img: topPipeImage, 
     x: pipeX,
@@ -95,4 +105,11 @@ function placePipes() {
   }
   pipeArray.push(topPipe);
   pipeArray.push(bottomPipe)
+}
+
+
+//collision
+
+function detectCollision(a,b){
+  return((a.x<b.x + b.width) &&( a.x + a.width >b.x) && (a.y <b.y+b.height) &&( a.y + a.height > b.y) )
 }
